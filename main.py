70b009b527b300d5e76fd7fb670ec1bccd1768d9
Althasandrian <Althasandrian@gmail.com>
2020-10-07 07:11:56 +0300

    Refactor response message creation to be dynamic.

--- main.py
+++ main.py
@@ -31,16 +31,9 @@ async def on_ready():
 @bot.command(name='prices')
 async def prices(ctx):
     data = updatePrices()
-
-    response = 'Purchasing all ore at following prices. Contract any amount to Lena70 Xiahou at the listed Buy Order Price in Berta, Maspah or Camal.\n\
-{} {} \n{} {} \n{} {} \n{} {} \n\
-{} {} \n{} {} \n{} {} \n{} {} \n\
-{} {} \n{} {} \n{} {} \n{} {} \n\
-{} {} \n{} {} \n{} {} \n{} {} \n'.format(\
-data[0][0], data[0][3], data[1][0], data[1][3], data[2][0], data[2][3], data[3][0], data[3][3],\
-data[4][0], data[4][3], data[5][0], data[5][3], data[6][0], data[6][3], data[7][0], data[7][3],\
-data[8][0], data[8][3], data[9][0], data[9][3], data[10][0], data[10][3], data[11][0], data[11][3],\
-data[12][0], data[12][3], data[13][0], data[13][3], data[14][0], data[14][3], data[15][0], data[15][3])
+    response = 'Purchasing all ore at following prices. Contract any amount to Lena70 Xiahou at the listed Buy Order Price in Berta, Maspah or Camal.\n'
+    for row in data:
+        response = response + '{} {} \n'.format(row[0], row[3])
     await ctx.send(response)
 
 def formatPrices(data):
